b013ee26aeacc72d68aebdcffe4840fd51ac02bc
Arturo Coronel <aoitsu3@gmail.com>
2017-02-13 07:11:34 -0800

    Added site-header component

---
+++ datyayu.xyz/components/SiteHeader/SiteHeader.vue
@@ -0,0 +1,118 @@
+<template>
+  <header class="header" :style="bgStyle">
+    <div class="header-overlay"></div>
+    <!-- Navigation component -->
+
+    <div class="header-content">
+      <h1 class="header-title">
+        <span> YAYU/ </span>
+        <span :class="headerColor")> {{ headerTitle }} </span>
+      </h1>
+
+      <h2 class="header-subtitle"> {{ headerSubtitle }} </h2>
+    </div>
+  </header>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      headerTitle: { type: String, required: true },
+      headerSubtitle: { type: String, required: true },
+      headerColor: { type: String, required: true },
+      bgImage: { type: String, required: true }
+    },
+
+    computed: {
+      bgStyle () {
+        return `background-image: url('${this.bgImage}')`
+      }
+    }
+  }
+</script>
+
+
+<style>
+/* Base */
+  .header {
+    height: 14em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .header-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,.5);
+  }
+
+  .header-content {
+    z-index: 1;
+    text-align: center;
+    color: white;
+  }
+
+  .header-title {
+    font-size: 2.8em;
+    margin: 0;
+  }
+
+  .header-subtitle {
+    font-size: 1em;
+    margin: 0;
+  }
+
+/* Tablet */
+  @media screen and (min-width: 480px) {
+    .header {
+      height: 16em;
+    }
+
+    .header-title {
+      font-size: 3.2em;
+    }
+
+    .header-subtitle {
+      font-size: 1.1em;
+    }
+  }
+
+/* Desktop */
+  @media screen and (min-width: 780px) {
+    .header {
+      height: 18em;
+    }
+
+    .header-title {
+      font-size: 3.6em;
+    }
+
+    .header-subtitle {
+      font-size: 1.2em;
+    }
+  }
+
+/* HD Desktop */
+  @media screen and (min-width: 1025px) {
+    .header {
+      height: 20em;
+    }
+
+    .header-title {
+      font-size: 4em;
+    }
+
+    .header-subtitle {
+      font-size: 1.3em;
+    }
+  }
+</style>
--- datyayu.xyz/pages/about.vue
+++ datyayu.xyz/pages/about.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>
-        <About></About>
-    </div>
+  <div>
+    <site-header
+      bg-image="/images/bg.png"
+      header-title="ME"
+      header-subtitle="Some info about me"
+      header-color="orange"
+    ></site-header>
+    <about></about>
+  </div>
 </template>
 
 
 <script>
   import About from '~components/About/About.vue'
+  import SiteHeader from '~components/SiteHeader/SiteHeader.vue'
 
   export default {
     components: {
-      About
+      About,
+      SiteHeader
     }
   }
 </script>
