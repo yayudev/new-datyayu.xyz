23e0671890518141c4149bafe457f43dc5b27131
Arturo Coronel <aoitsu3@gmail.com>
2017-02-17 02:47:13 -0800

    Updated about copy

--- i18n/pages/about.js
+++ i18n/pages/about.js
@@ -6,13 +6,13 @@ export default {
 
       introTitle: 'Me',
 
-      intro: 'Hi. I\'m Arturo Coronel (you can call me Yayu). I\'m 21 años, I live at Tijuana, MX and I ❤ web development.',
+      intro: 'Hi! I\'m Arturo Coronel (you can call me Yayu). I\'m 21 years old, I live at Tijuana, MX and I ❤ web development.',
 
       work: 'I work as a frontend developer, I study Computer Engineering, help on research projects and sometimes I do freelance. But my main job is to learn and share every single day.',
 
-      openSource: 'I love to share almost every projects I made on my Github account, with MIT license, so anyone can use my work as inspiration. I also publish there tools and modules that I\'ve developed to make my job easier; in that way other people can be more productive and learn from what I do as well',
+      openSource: 'I love to share almost every projects I made on my Github account, with MIT license, so anyone can use my work as inspiration. I also publish there tools and modules that I\'ve developed to make my job easier; in that way other people can be more productive and learn from what I do as well.',
 
-      hobbies: 'My hobbies are watching anime, listening to podcasts, learn about my favorite voice actresses, write about frontend and help other people to improve their skills',
+      hobbies: 'My hobbies are watching anime, listening to podcasts, learn about my favorite voice actresses, write about frontend and help other people to improve their skills.',
 
       contactTitle: 'Contact',
 
@@ -27,13 +27,13 @@ export default {
 
       introTitle: 'YO',
 
-      intro: 'Hola. Soy Arturo Coronel (puedes llamarme Yayu), tengo 21 años, vivo en Tijuana, MX y amo el desarrollo web ♥.',
+      intro: 'Hola. Soy Arturo Coronel (puedes llamarme Yayu), tengo 21 años, vivo en Tijuana, MX y amo el desarrollo web ❤.',
 
-      work: 'Trabajo haciendo frontend, soy estudiante de ingeniería en computación en la UABC, ayudo en proyectos de investigación y de vez en cuando hago freelance. Pero ante todo me dedico a aprender por mi cuenta todos los días.',
+      work: 'Trabajo como desarrollador de front-end, estudio ingeniería en computación, ayudo en proyectos de investigación y hago freelance. Pero mi trabajo principal es aprender y compartir todos los días.',
 
       openSource: 'Me gusta compartir casi todo lo que hago en mi cuenta de Github, con licencia de MIT para que cualquiera pueda inspirarse; así como herramientas y librerías que he desarrollado para hacer más fácil mi trabajo, así otros pueden hacer más fácil el suyo y aprender de mi código.',
 
-      hobbies: 'Mis hobbies son ver anime, escuchar podcasts, stalkear aprender acerca de mis actrices de voz favoritas, escribir acerca de javascript y ayudar a los demás a aprender.',
+      hobbies: 'Mis hobbies son ver anime, escuchar podcasts, aprender acerca de mis actrices de voz favoritas, escribir acerca de frontend y ayudar a los demás a mejorar sus habilidades.',
 
       contactTitle: 'Contacto',
 
