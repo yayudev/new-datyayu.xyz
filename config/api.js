87e7e1a53e16b19b1dbd7e7f8a5f900550544a97
Arturo Coronel <aoitsu3@gmail.com>
2017-02-20 10:40:58 -0800

    Changed api to use https

--- config/api.js
+++ config/api.js
@@ -1,2 +1,2 @@
-export const BASE_ENDPOINT = 'http://blog-api.datyayu.xyz/wp-json/wp/v2'
+export const BASE_ENDPOINT = 'https://blog-api.datyayu.xyz/wp-json/wp/v2'
 export const POSTS_ENDPOINT = `${BASE_ENDPOINT}/posts`
