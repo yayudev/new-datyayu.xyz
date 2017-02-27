3579282e1409e8ba38134892733f329875cd7abb
Arturo Coronel <aoitsu3@gmail.com>
2017-02-27 04:32:12 -0800

    Updated about copy

--- i18n/pages/about.js
+++ i18n/pages/about.js
@@ -6,17 +6,44 @@ export default {
 
       introTitle: 'Me',
 
-      intro: 'Hi! I\'m Arturo Coronel (you can call me Yayu). I\'m 21 years old, I live at Tijuana, MX and I ❤ web development.',
+      intro: `
+        Hi! I'm Arturo Coronel (you can call me Yayu).
+        I'm 21 years old, I live at Tijuana, MX and
+        I ❤ web development.
+      `,
 
-      work: 'I work as a frontend developer, I study Computer Engineering, help on research projects and sometimes I do freelance. But my main job is to learn and share every single day.',
+      work: `
+        I work as a web developer, focusing mainly on the front-end, UI
+        design and UX areas. My favorite things about them are optimizing
+        the css usage to improve the page performance. I'm also experienced
+        with major css libraries like bootstrap, materialize and foundation;
+        as well as javascript frameworks like Angular, Angular2, ember, polymer
+        and, as my main foucs. React+redux and Vue.js.
+      `,
 
-      openSource: 'I love to share almost every projects I made on my Github account, with MIT license, so anyone can use my work as inspiration. I also publish there tools and modules that I\'ve developed to make my job easier; in that way other people can be more productive and learn from what I do as well.',
+      openSource: `
+         I love to share almost every projects I made on my
+         Github account, with MIT license, so anyone can
+         use my work as inspiration. I also publish there
+         tools and modules that I've developed to make my
+         job easier; in that way other people can be more
+         productive and learn from what I do as well.
+      `,
 
-      hobbies: 'My hobbies are watching anime, listening to podcasts, learn about my favorite voice actresses, write about frontend and help other people to improve their skills.',
+      hobbies: `
+        My hobbies are watching anime, listening to podcasts,
+        learn about my favorite voice actresses, write about frontend
+        and help other people to improve their skills.
+      `,
 
       contactTitle: 'Contact',
 
-      contactText: 'Want to know more about me or contract my services? Do you have any idea or suggestion to improve this site? Want to talk about web develoment or chinese cartoons? Contact me!'
+      contactText: `
+        Want to know more about me or contract my services?
+        Do you have any idea or suggestion to improve this site?
+        Want to talk about web develoment or chinese cartoons?
+        Contact me!
+      `
     }
   },
 
@@ -27,17 +54,44 @@ export default {
 
       introTitle: 'YO',
 
-      intro: 'Hola. Soy Arturo Coronel (puedes llamarme Yayu), tengo 21 años, vivo en Tijuana, MX y amo el desarrollo web ❤.',
+      intro: `
+        Hola. Soy Arturo Coronel (puedes llamarme Yayu), tengo 21 años,
+        vivo en Tijuana, MX y amo el desarrollo web ❤.
+      `,
 
-      work: 'Trabajo como desarrollador de front-end, estudio ingeniería en computación, ayudo en proyectos de investigación y hago freelance. Pero mi trabajo principal es aprender y compartir todos los días.',
+      work: `
+        Me dedico a hacer desarrollo web, principalmente en las areas de
+        front-end, diseño de interfaces y experiencia de usuario.
+        Mis cosa favorita de estas areas son optimizar el
+        uso de css para mejorar el rendimiento de la página.
+        También tengo experiencia trabajando con librerias de css como
+        bootstrap, materialize, foundation; y frameworks de javascript
+        como Angular, Angular2, ember, polymer y, como mi enfoque principal,
+        React+Redux y Vue.js.
+      `,
 
-      openSource: 'Me gusta compartir casi todo lo que hago en mi cuenta de Github, con licencia de MIT para que cualquiera pueda inspirarse; así como herramientas y librerías que he desarrollado para hacer más fácil mi trabajo, así otros pueden hacer más fácil el suyo y aprender de mi código.',
+      openSource: `
+        Me gusta compartir casi todo lo que hago en mi cuenta de Github,
+        con licencia de MIT para que cualquiera pueda inspirarse;
+        así como herramientas y librerías que he desarrollado para hacer
+        más fácil mi trabajo, así otros pueden hacer más fácil el suyo y
+        aprender de mi código.
+      `,
 
-      hobbies: 'Mis hobbies son ver anime, escuchar podcasts, aprender acerca de mis actrices de voz favoritas, escribir acerca de frontend y ayudar a los demás a mejorar sus habilidades.',
+      hobbies: `
+        Mis hobbies son ver anime, escuchar podcasts, aprender acerca de
+        mis actrices de voz favoritas, escribir acerca de frontend y ayudar
+        a los demás a mejorar sus habilidades.
+      `,
 
       contactTitle: 'Contacto',
 
-      contactText: '¿Quieres saber más de mí o contratar mis servicios? ¿Tienes alguna idea o sugerencia para mejorar este blog? ¿Deseas platicar de tecnologías web o monas chinas? ¡Contáctame!'
+      contactText: `
+        ¿Quieres saber más de mí o contratar mis servicios?
+        ¿Tienes alguna idea o sugerencia para mejorar este blog?
+        ¿Deseas platicar de tecnologías web o monas chinas?
+        ¡Contáctame!
+      `
     }
   }
 }
