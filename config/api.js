061b86859ba3e71c593ffd5113b4404be6ad38f5
Arturo Coronel <aoitsu3@gmail.com>
2017-06-09 00:40:48 -0700

    Fixed error with api url

--- config/api.js
+++ config/api.js
@@ -1,8 +1,7 @@
 // Base endpoint
-const HOST_URL =
-// process.env.NODE_ENV === 'production'
-//   ? 'https://datyayu.xyz'
-  'http://localhost:3000'
+const HOST_URL = process.env.NODE_ENV === 'production'
+  ? 'https://datyayu.xyz'
+  : 'http://localhost:3000'
 
 export const POSTS_ENDPOINT = `${HOST_URL}/api/posts`
 export const TAGS_ENDPOINT = `${HOST_URL}/api/tags`
