fb7c408f6828b1ea54cdbc9495ac77b289723f6f
Arturo Coronel <aoitsu3@gmail.com>
2017-06-17 06:47:35 -0700

    Updated phrases at home page

--- i18n/pages/home.js
+++ i18n/pages/home.js
@@ -16,9 +16,10 @@ export default {
         'Collects mangas',
         'Puts stickers on his laptop',
         'Knows how to juggle',
+        'Idol otaku',
         'Hated the Angular2 RC fase',
         'Loves to share knowledge',
-        'The video is from YuruYuri',
+        'The bg is from Yuru Yuri',
         'EMs are the superior css unit',
         'Loves to animate using css',
         'Shares side-projects on github',
@@ -26,9 +27,11 @@ export default {
         'Believes RWD is a must-have',
         'Using React since early 2015',
         'Fell in love with Vue.js',
+        'Cares about web performance',
         'WORDS, WORDS, WORDS.',
+        'Plays too many mobile games',
         'Hi',
-        'Currently A3 on Shadowverse'
+        'Currently AA1 on Shadowverse'
       ]
     }
   },
@@ -52,7 +55,8 @@ export default {
         'Sabe hacer malabares',
         'Odió la fase RC de Angular2',
         'Le encanta compartir info',
-        'El video es de YuruYuri',
+        'El fondo es de Yuru Yuri',
+        'Idol otaku',
         'EM es la unidad superior',
         'Adora animar cosas en CSS',
         'Comparte proyectos en Github',
@@ -60,8 +64,10 @@ export default {
         'Cree que RWD es obligatorio',
         'Usando react desde el 2015',
         'Se ha enamorado de Vue.js',
+        'Juega demasiado juegos mobiles',
+        'Le importa el rendimiento web',
         'Hola',
-        'Actualmente A3 en Shadowverse'
+        'Actualmente AA1 en Shadowverse'
       ]
     }
   }
